8c5d6105d4c5cc8b5240179e886e4cf0f35d54a2
Eugene Yuriev <juriev_gek@mail.ru>
2016-06-14 17:43:46 +0300

    Исправил замечания. Шаг 8

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,23 @@ navToggle.addEventListener('click', function() {
     navUser.classList.remove('user-nav--opened');
   }
 });
+
+// Создание обработчика для события window.onLoad
+YMaps.jQuery(function () {
+    // Создание экземпляра карты и его привязка к созданному контейнеру
+    var map = new YMaps.Map(YMaps.jQuery("#yandex-map")[0]);
+    map.setCenter(new YMaps.GeoPoint(30.323054, 59.938631), 17);
+    map.addControl(new YMaps.ToolBar());
+    map.addControl(new YMaps.Zoom());
+
+    // Создание стиля для значка метки
+    var s = new YMaps.Style();
+    s.iconStyle = new YMaps.IconStyle();
+    s.iconStyle.href = "../img/icon-map-pin.svg";
+    s.iconStyle.size = new YMaps.Point(70, 100);
+    s.iconStyle.offset = new YMaps.Point(-35, -100);
+
+    // Создание метки и добавление ее на карту
+    var placemark = new YMaps.Placemark(map.getCenter(), {style: s});
+    map.addOverlay(placemark);
+});
